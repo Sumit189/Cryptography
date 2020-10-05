099b5f029ee97f3012d4853fb83250db87dbaefc
Sumit <sumit.18.paul@gmail.com>
2020-10-05 18:51:58 +0530

    Update columnar_transposition.cpp

--- C++/columnar_transposition.cpp
+++ C++/columnar_transposition.cpp
@@ -1,1 +1,29 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+string encrypt(string message,string key,int pattern []){
+	int row=ceil((float)message.size()/(float)key.size());
+	
+	char arr[row][key.size()];
+	int k=0;
+	for(int i=0;i<row;i++){
+		for(int j=0;j<key.size();j++){
+			if(k>=message.size()){
+				arr[i][pattern[j]]='x';
+			}
+			else{
+			arr[i][pattern[j]]=(char)message[k];
+		    }
+			k++;
+		}
+	}
+	
+		string res="";
+		for(int i=0;i<key.size();i++){
+		for(int j=0;j<row;j++){
+			res+=arr[j][i];
+		}
+	}
+	return res;
+}
